1da9ebae4cd58f4c0739d055da83fb501f546751
PABITRA\PABITRA BERA <pabitra.debtech@gmail.com>
2025-09-02 18:48:53 +0530

    now tailwind css is working fine but now need to design using this tailwind css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,6 +3,8 @@ import React from "react";
 export default function Home() {
   return (
     <div style={{ padding: "20px" }}>
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind</h1>
+
       <h1>Welcome to My Portfolio</h1>
       <p>This is the public view of my portfolio website.</p>
     </div>
